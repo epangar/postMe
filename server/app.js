398a73814d84ea225a75522be85dc1309658774e
Tanis Pan <e.pan.gar@gmail.com>
2019-07-04 18:05:48 +0200

    Resolver dependencias

--- server/app.js
+++ server/app.js
@@ -11,13 +11,14 @@ const path         = require('path');
 
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/server', {useNewUrlParser: true})
+  .connect('mongodb://localhost/myRemedy', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
   .catch(err => {
     console.error('Error connecting to mongo', err)
   });
+  
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
