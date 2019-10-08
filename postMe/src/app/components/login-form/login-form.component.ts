c5e114a57c6d0877b088425a1d4de9e6826227d4
Tanis Pan <e.pan.gar@gmail.com>
2019-10-08 15:17:31 +0200

    Servicio de Sesión

--- postMe/src/app/components/login-form/login-form.component.ts
+++ postMe/src/app/components/login-form/login-form.component.ts
@@ -18,7 +18,6 @@ export class LoginFormComponent implements OnInit {
   login() {
     
     console.log(this.username, this.password);
-    debugger
     this.sessionService.login(this.username, this.password).subscribe();
   }
 
--- postMe/src/app/services/session.service.ts
+++ postMe/src/app/services/session.service.ts
@@ -30,6 +30,7 @@ export class SessionService {
   }
 
   private configureUser(set=false){
+    
     return (user) => {
       if(set){
         this.user = user;
@@ -57,6 +58,7 @@ export class SessionService {
   }
 
   login(username:string, password:string):Observable<any>{
+    
     return this.http.post(`${this.BASEURL}/api/auth/login`, {username,password},this.options)
       .pipe(map(response =>response))
       .pipe(map(this.configureUser(true)))
